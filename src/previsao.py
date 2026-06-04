d61267e659b2c591df90cfdeb4975a823ea9359e
Victor de Camargo Gomes <147652397+VictorCamargo344@users.noreply.github.com>
2026-06-04 11:52:01 -0300

    Update previsao.py
    regressao_linear e prever_proximo_ponto

--- src/previsao.py
+++ src/previsao.py
@@ -2,11 +2,22 @@ import config
 
 
 def regressao_linear(xs, ys):
-    pass
+    n = len(xs)
+    soma_x  = sum(xs)
+    soma_y  = sum(ys)
+    soma_xy = sum(xs[i] * ys[i] for i in range(n))
+    soma_x2 = sum(xi ** 2 for xi in xs)
+
+    a = (n * soma_xy - soma_x * soma_y) / (n * soma_x2 - soma_x ** 2)
+    b = (soma_y - a * soma_x) / n
+
+    return round(a, 4), round(b, 4)
 
 
 def prever_proximo_valor(a, b, x_proximo):
-    pass
+    y_previsto = a*x_proximo + b
+
+    return round(y_previsto, 4)
 
 
 def prever_reserva_energetica(telemetria):
