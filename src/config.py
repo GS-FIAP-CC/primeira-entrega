73cb302540970636ba5e91e9b2d2b179d8f2e9c6
Eduardo Mota <eduardolsmota@gmail.com>
2026-06-06 19:24:11 -0300

    ajuste no config.py

--- src/config.py
+++ src/config.py
@@ -1,26 +1,14 @@
-
-MODULOS_CRITICOS = [
-    "suporte_vida",
-    "energia",
-    "comunicacao",
-    "habitat",
-    "laboratorio",
-    "armazenamento",
-]
+MODULO_VITAL = "suporte_vida"
 
 RESERVA_NORMAL_MIN = 50
 RESERVA_ALERTA_MIN = 25
+RESERVA_MODERADA_MAX = 60
 
 COMUNICACAO_NORMAL_MIN = 70
 COMUNICACAO_ALERTA_MIN = 30
+
 TEMP_INTERNA_MIN = 18
 TEMP_INTERNA_MAX = 26
 
-RADIACAO_NIVEIS = ["normal", "alerta", "alta"]
-
-SEVERIDADES = ["normal", "alerta", "critico"]
-PRIORIDADES = ["media", "alta", "critica"]
-
-ARQUIVO_DADOS_PADRAO = "data/dados.csv"
-
-PREVISAO_CICLOS_FUTUROS = 1 
+RADIACAO_ALERTA_MIN = 0.3
+RADIACAO_CRITICO_MIN = 0.6
